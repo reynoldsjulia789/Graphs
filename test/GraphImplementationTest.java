b46e13a5db0c7af4d059a172df7a9c464861f9b8
reynoldsjulia789 <jreynolds21@ewu.edu>
2026-05-18 11:23:26 -0700

    Setup

---
+++ test/GraphImplementationTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GraphImplementationTest
+{
+    @Nested
+    @DisplayName("Digraph Tests")
+    class Digraph
+    {
+
+    }
+
+    @Nested
+    @DisplayName("Adjacency List Tests")
+    class AdjList
+    {
+
+    }
+
+    @Nested
+    @DisplayName("Adjacency Matrix Tests")
+    class AdjMatrix
+    {
+
+    }
+}
