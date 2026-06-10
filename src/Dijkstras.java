b1f99b23b45268a9b0609959f0b32a1de455019d
reynoldsjulia789 <jreynolds21@ewu.edu>
2026-06-09 20:17:01 -0700

    trying to implement dijkstras

--- src/Dijkstras.java
+++ src/Dijkstras.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
 /**
  * A class implementing Dijkstra's algorithm to search a graph for the shortest path from
  * source to destination.
@@ -11,8 +14,44 @@ public class Dijkstras implements Search
 
     public static Path search(String src, String dest, Digraph graph)
     {
+        PriorityQueue<Vertex> q;
+        ArrayList<String>     nodes, edges;
 
+        // For each node v != source,
+        // set v.cost = infinity and v.known = false
+        // set source.cost = 0 and source.known = true
+        // while there are unknown nodes in the graph
+        // select the unknown node v with the lowest cost
+        // mark v as known
+        // for each edge(v,u) with weight w,
+        // c1 = v.cost + w (cost of best path through v to u)
+        // c2 = u.cost (cost of best path to u previously known)
+        // if (c1 < c2)
+        // u.cost = c1
+        // u.path = v
     }
 
 
+    private class Vertex implements Comparable<Vertex>
+    {
+        private String  name;
+        private boolean known;
+        private Integer cost;
+        private Vertex  backtrace;
+
+        private Vertex(String name)
+        {
+            this.name = name;
+        }
+
+        /**
+         * @param other the object to be compared.
+         * @return int representing ordering
+         */
+        @Override
+        public int compareTo(Vertex other)
+        {
+            return this.cost - other.cost;
+        }
+    }
 }
